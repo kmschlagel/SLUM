e3b73145928581290492904ca4c88f9c7ef0acb4
kateschlagel <kmaillette@gmail.com>
2014-05-18 09:01:29 -0500

    Configured sendmail to match registration form. Added thank-you page for registration confirmation.

--- sendmail.php
+++ sendmail.php
@@ -2,22 +2,30 @@
 $name = $_POST['name'];
 
 
-$to = 'walkadock@q.com';
-$subject = 'New Customer ';
+$to = 'kmaillette@gmail.com';
+$subject = 'New VBS Student ';
 
 $message = "Name: $name\n";
-$message .= 'Email: ' . $_POST['email'] . "\n";
-$message .= 'City: ' . $_POST['city'] . "\n";
-$message .= 'State: ' . $_POST['state'] . "\n";
+$message .= 'Parent Name: ' . $_POST['pname'] . "\n";
+$message .= 'Address: ' . $_POST['address'] . "\n";
 $message .= 'Phone: ' . $_POST['phone'] . "\n";
-$message .= 'Docks: ' . $_POST['docks'] . "\n";
-$message .= 'Lifts: ' . $_POST['boat_lifts'] . "\n";
-$message .= 'Used Docks: ' . $_POST['used_docks'] . "\n";
-$message .= 'Used Lifts: ' . $_POST['used_lifts'] . "\n";
-$message .= 'Services: ' . $_POST['services'] . "\n";
-$message .= 'Welding/Fabrication: ' . $_POST['welding_fabrication'] . "\n";
-$message .= 'Other: ' . $_POST['other'] . "\n";
-$message .= 'Other Information: ' . $_POST['other_info'] . "\n";
+$message .= 'Emergency Phone: ' . $_POST['ephone'] . "\n";
+$message .= 'Email: ' . $_POST['email'] . "\n";
+$message .= 'Child Name ' . $_POST['cname1'] . "\n";
+$message .= 'Child Age: ' . $_POST['age1'] . "\n";
+$message .= 'Second Child Name ' . $_POST['cname2'] . "\n";
+$message .= 'Second Child Age: ' . $_POST['age2'] . "\n";
+$message .= 'Third Child Name ' . $_POST['cname3'] . "\n";
+$message .= 'Third Child Age: ' . $_POST['age3'] . "\n";
+$message .= 'Fourth Child Name ' . $_POST['cname4'] . "\n";
+$message .= 'Fourth Child Age: ' . $_POST['age4'] . "\n";
+$message .= 'Fifth Child Name ' . $_POST['cname5'] . "\n";
+$message .= 'Fifth Child Age: ' . $_POST['age5'] . "\n";
+$message .= 'Sixth Child Name ' . $_POST['cname6'] . "\n";
+$message .= 'Sixth Child Age: ' . $_POST['age6'] . "\n";
+$message .= 'Special Needs: ' . $_POST['needs'] . "\n";
+$message .= 'How did you hear?: ' . $_POST['hear_about_VBS'] . "\n";
+$message .= 'I would like information: ' . $_POST['information'] . "\n";
 $message .= "\n";
 
 
@@ -27,5 +35,5 @@ $headers =	'From: <' . $_POST['email'] . '> '. $name ."\r\n" .
 
 
 mail($to, $subject, $message, $headers);
-header('location: http://www.walkadock.com/thankyou.html');
+header('location: http://www.walkadock.com/thankyou.php');
 ?>
---
+++ thankyou.php
@@ -0,0 +1,55 @@
+<?php include 'header.php' ?>
+
+<div id="content">
+    <div id="main">
+        <img src="images/VBS/logo-workshopofwonders-cropped.jpg" width="185" height="146" style="float: right;">
+
+        <h3>Vacation Bible School Registration</h3>
+
+        <p>Invite your children, grandchildren, and friends to Workshop of Wonders this summer at Vacation Bible School!
+            In Workshop of Wonders, kids from ages three through 6th Grade will experience
+            the love of Jesus through Bible stories, games, fun crafts, music, discovery time, and dinner.<br><span
+                class="bold">Monday August 18<sup>th</sup> - Thursday August 21<sup>st</sup></span><br><span
+                class="bold">6:00pm - 8:00pm daily</span></p>
+
+        <h3 style="color:#C9A68F; margin-top: 10px;">Thank you for Registering.</h3>
+        <p class="bold">We look forward to seeing you!</p>
+    </div>
+    <!--end main-->
+    <div id="sidebar">
+        <div id="worship">
+            <h4>Sunday Worship</h4>
+            <ul>
+                <li>9:00am Sunday School <br>(Grades 6-12 / Adult)</li>
+                <li>10:00am Worship Service</li>
+                <li>11:00am Coffee Hour</li>
+            </ul>
+        </div>
+        <!-- /worship -->
+        <div id="findus">
+            <h4>Find Us</h4>
+
+            <p>5399 Geneva Ave. North<br>
+                Oakdale, MN 55128</p>
+
+            <p>651.773.9397</p>
+
+            <div class="map">
+                <iframe id="gmap" width="225" height="175" frameborder="0" scrolling="no" marginheight="0"
+                        marginwidth="0"
+                        src="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5399+Geneva+Avenue+North+Oakdale,+Minnesota+55128&amp;aq=&amp;sll=44.934383,-93.112741&amp;sspn=0.067201,0.169086&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=5399+Geneva+Ave+N,+Oakdale,+Minnesota+55128&amp;z=14&amp;ll=45.026986,-92.983909&amp;output=embed"></iframe>
+                <p>
+                    <a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=5399+Geneva+Avenue+North+Oakdale,+Minnesota+55128&amp;aq=&amp;sll=44.934383,-93.112741&amp;sspn=0.067201,0.169086&amp;t=h&amp;ie=UTF8&amp;hq=&amp;hnear=5399+Geneva+Ave+N,+Oakdale,+Minnesota+55128&amp;z=14&amp;ll=45.026986,-92.983909"
+                       target="_blank">View Larger Map</a></p>
+            </div>
+            <!--end map-->
+        </div>
+        <!-- /findus -->
+    </div>
+    <!--end sidebar-->
+</div> <!-- /content -->
+<div id="footer">
+    <h4>Contact Us</h4>
+
+    <?php include 'footer.php' ?>
+
